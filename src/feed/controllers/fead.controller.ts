37b04b2aededd78bfb786273d8e85ef693c26ac0
hashika <m.g.hashikamaduranga@gmail.com>
2021-06-13 21:54:01 +0530

    create nestjs for create api for ssave data in postgress

---
+++ src/feed/controllers/fead.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UpdateResult } from 'typeorm';
+import { FeedPost } from '../model/post.interface';
+import { FeedService } from './fead.service';
+
+@Controller('feed')
+export class FeadController {
+    constructor( private feedService: FeedService){
+    }
+
+    @Post()
+    create(@Body() post: FeedPost): Observable<FeedPost>{
+        return this.feedService.createPost(post)
+    }
+
+    @Get()
+    findAll(): Observable<FeedPost[]>{
+        return this.feedService.findAllPosts();
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body() postFeed: FeedPost
+    ): Observable<UpdateResult>{
+        return this.feedService.updatePost(id,postFeed)
+    }
+}
