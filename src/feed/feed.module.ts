1a0bd3ec6ef3963e2537fb336af6e1ccc49ea55b
hashika <m.g.hashikamaduranga@gmail.com>
2021-10-28 10:43:23 +0530

    chnages

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,12 +3,19 @@ import { FeedService } from './controllers/fead.service';
 import { FeadController } from './controllers/fead.controller';
 import { FeedPostEntity } from './model/post.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/user/user/user.constant';
 
 @Module({
   imports:[
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
     TypeOrmModule.forFeature([FeedPostEntity])
   ],
   providers: [FeedService],
-  controllers: [FeadController]
+  controllers: [FeadController],
+  exports:[ FeedService ]
 })
 export class FeedModule {}
